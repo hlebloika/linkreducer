a2c85b2528e9fb24c30a74176aa6565e04ab4306
hlebloika <gleb.loiko@gmail.com>
2022-04-08 22:17:00 +0000

    dry implementation of links redirection

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import string, random
 
 app = Flask(__name__)
+match_urls = {}
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main_page():
+    if request.method == 'POST':
+        original_url = request.form['input_link']
+        link_id = "".join([random.choice(string.ascii_letters) for i in range(6)])
+        short_url="http://35.179.15.24/" + link_id
+        match_urls[link_id] = original_url
+        return "<p>" + short_url + "</p>"
     return render_template("main.html")
+
+@app.route("/<link_id>")
+def redirect_to_original_url(link_id):
+    url_to = match_urls[link_id]
+    return redirect(url_to)
+
